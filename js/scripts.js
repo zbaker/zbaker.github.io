46c995465bfa983419c904672a5298ebbfebf4ef
zbaker <zcbaker@ymail.com>
2015-06-10 00:29:21 -0700

    Added Styling and Picture files

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,37 @@
 // My Scripts
+
+$('.all').click(function() {
+	console.log("Click");
+});
+
+/*
+// Click Photography
+$('.ps').click( function() {
+  
+  if ( $('img').hasClass('hide') ) {
+    $('img').removeClass('hide'); 
+  }
+  
+  $('.illustrator').addClass('hide');
+
+});
+
+// Click Illustration
+$('.ai').click( function() {
+  
+    if ( $('img').hasClass('hide') ) {
+    	$('img').removeClass('hide'); 
+    }
+    $('.photoshop').addClass('hide');
+  
+});
+
+// Show All 
+$('.all').click( function() {
+  
+  if ( $('img').hasClass('hide') ) {
+    $('img').removeClass('hide'); 
+  };
+  
+});
+*/
